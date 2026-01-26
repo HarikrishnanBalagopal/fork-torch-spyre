b2d93f0771b4fb73c920a0b20d55c9fff866129d
Thomas Ortner <boh@zurich.ibm.com>
2026-01-26 22:10:11 +0100

    Reenable cpu device for torch.compile (#387)
    *) Ensure that standard PyTorch devices are registered
    *) Execute the `_pre_fusion_custom_pass` only in case of spyre device
    arguments
    
    ---------
    
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- torch_spyre/_inductor/__init__.py
+++ torch_spyre/_inductor/__init__.py
@@ -31,6 +31,11 @@ def _autoload():
         register_backend_for_device,
         register_device_op_overrides,
     )
+
+    # Register in-tree CPU and CUDA device
+    from torch._inductor.codegen import cpu_device_op_overrides  # noqa: F401  # usort: skip
+    from torch._inductor.codegen.cuda import device_op_overrides  # noqa: F401  # usort: skip
+
     from torch_spyre.utils.device_op_overrides import SpyreDeviceOpOverrides
 
     register_device_op_overrides(
@@ -70,13 +75,20 @@ def _autoload():
     torch._inductor.virtualized.V.set_choices_handler(SpyreHeuristics())
 
     # Customize inductor configuration
-    from .passes import CustomPrePasses, CustomPostPasses, scheduler_passes
+    from .passes import (
+        CustomPrePasses,
+        CustomPostPasses,
+        scheduler_passes,
+        _maybe_run_pass,
+    )
 
     torch._inductor.config.split_reductions = False
     torch._inductor.config.benchmark_harness = False
     torch._inductor.config.post_grad_custom_pre_pass = CustomPrePasses()
     torch._inductor.config.post_grad_custom_post_pass = CustomPostPasses()
-    torch._inductor.config._pre_fusion_custom_pass = scheduler_passes
+    torch._inductor.config._pre_fusion_custom_pass = lambda nodes: _maybe_run_pass(
+        scheduler_passes, nodes
+    )
     # Adding this configuration in so as to avoid the optimization of turning small matmuls into non-matmuls
     # found here: https://github.com/pytorch/pytorch/blob/main/torch/_inductor/ir.py#L1580
     torch._inductor.config.unroll_reductions_threshold = 1
